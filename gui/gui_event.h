65a4daf3d1ed6a12b9e7d449405f180775ea7380
artem.lab <artem.lab@gmail.com>
2015-11-30 11:22:59 +0500

    gui_event accept/ignore

--- gui/gui_event.h
+++ gui/gui_event.h
@@ -7,6 +7,7 @@
 
 #include "gui.h"
 #include <stddef.h>
+#include <stdbool.h>
 
 typedef enum _Gui_Event_Type {
     GUI_EVENT_TYPE_NONE = 0, //!< Отсутствующее событие.
@@ -17,26 +18,28 @@ typedef enum _Gui_Event_Type {
 //! Структура события графического интерфейса.
 typedef struct _Gui_Event {
     gui_event_type_t type; //!< Тип события.
+    bool accepted; //!< Принято ли событие.
 } gui_event_t;
 
 //! Структура события изменения размера.
 typedef struct _Gui_Resize_Event {
     gui_event_t super; //!< Суперкласс.
-    size_t width; //!< Новая ширина.
-    size_t height; //!< Новая высота.
+    graphics_size_t width; //!< Новая ширина.
+    graphics_size_t height; //!< Новая высота.
 } gui_resize_event_t;
 
 //! Структура события перерисовки.
 typedef struct _Gui_Repaint_Event {
     gui_event_t super; //!< Суперкласс.
-    int x; //!< Координата X области перерисовки.
-    int y; //!< Координата Y области перерисовки.
-    size_t width; //!< Ширина области перерисовки.
-    size_t height; //!< Высота области перерисовки.
+    graphics_pos_t x; //!< Координата X области перерисовки.
+    graphics_pos_t y; //!< Координата Y области перерисовки.
+    graphics_size_t width; //!< Ширина области перерисовки.
+    graphics_size_t height; //!< Высота области перерисовки.
 } gui_repaint_event_t;
 
-
+//! Приводит указатель event к типу события.
 #define GUI_EVENT(event) ((gui_event_t*)(event))
+
 /**
  * Инициализирует событие.
  * @param event Событие.
@@ -44,26 +47,59 @@ typedef struct _Gui_Repaint_Event {
 ALWAYS_INLINE static void gui_event_init(gui_event_t* event, gui_event_type_t type)
 {
     event->type = type;
+    event->accepted = false;
 }
 
+/**
+ * Получает флаг принятия события.
+ * @param event Событие.
+ * @return Флаг принятия события.
+ */
+ALWAYS_INLINE static bool gui_event_accepted(gui_event_t* event)
+{
+    return event->accepted;
+}
+
+/**
+ * Принимает событие.
+ * @param event Событие.
+ */
+ALWAYS_INLINE static void gui_event_accept(gui_event_t* event)
+{
+    event->accepted = true;
+}
+
+/**
+ * Игнорирует событие.
+ * @param event Событие.
+ */
+ALWAYS_INLINE static void gui_event_ignore(gui_event_t* event)
+{
+    event->accepted = false;
+}
+
+//! Приводит указатель event к типу события изменения размера.
 #define GUI_RESIZE_EVENT(event) ((gui_resize_event_t*)(event))
+
 /**
  * Инициализирует событие изменения размера.
  * @param event Событие изменения размера.
  */
-ALWAYS_INLINE static void gui_resize_event_init(gui_resize_event_t* event, size_t width, size_t height)
+ALWAYS_INLINE static void gui_resize_event_init(gui_resize_event_t* event, graphics_size_t width, graphics_size_t height)
 {
     gui_event_init(GUI_EVENT(event), GUI_EVENT_TYPE_RESIZE);
     event->width = width;
     event->height = height;
 }
 
+//! Приводит указатель event к типу события перерисовки..
 #define GUI_REPAINT_EVENT(event) ((gui_repaint_event_t*)(event))
+
 /**
  * Инициализирует событие перерисовки.
  * @param event Событие перерисовки.
  */
-ALWAYS_INLINE static void gui_repaint_event_init(gui_repaint_event_t* event, int x, int y, size_t width, size_t height)
+ALWAYS_INLINE static void gui_repaint_event_init(gui_repaint_event_t* event, graphics_pos_t x, graphics_pos_t y, graphics_size_t width, graphics_size_t height)
 {
     gui_event_init(GUI_EVENT(event), GUI_EVENT_TYPE_REPAINT);
     event->x = x;

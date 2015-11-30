27322de17501ffb4126855480714819ccff917c3
artem.lab <artem.lab@gmail.com>
2015-11-30 16:35:42 +0500

    gui_widget struct get/setters

--- gui/gui_widget.c
+++ gui/gui_widget.c
@@ -1,1 +1,84 @@
 #include "gui_widget.h"
+#include "utils/utils.h"
+
+
+static widget_id_t next_widget_id = 1;
+
+
+void gui_widget_resize_event(gui_widget_t* widget, gui_resize_event_t* event)
+{
+    gui_widget_resize(widget, event->width, event->height);
+}
+
+void gui_widget_repaint_event(gui_widget_t* widget, gui_repaint_event_t* event)
+{
+}
+
+err_t gui_widget_init(gui_widget_t* widget, gui_t* gui)
+{
+    RETURN_ERR_IF_FAIL(gui_object_init(GUI_OBJECT(widget), gui));
+    
+    widget->id = next_widget_id ++;
+    widget->visible = true;
+    widget->x = 0;
+    widget->y = 0;
+    widget->width = 0;
+    widget->height = 0;
+    widget->resize_event = gui_widget_resize_event;
+    widget->repaint_event = gui_widget_repaint_event;
+    
+    return E_NO_ERROR;
+}
+
+err_t gui_widget_init_parent(gui_widget_t* widget, gui_t* gui, gui_widget_t* parent)
+{
+    RETURN_ERR_IF_FAIL(gui_object_init_parent(GUI_OBJECT(widget), gui, GUI_OBJECT(parent)));
+    
+    widget->id = next_widget_id ++;
+    widget->visible = true;
+    widget->x = 0;
+    widget->y = 0;
+    widget->width = 0;
+    widget->height = 0;
+    widget->resize_event = gui_widget_resize_event;
+    widget->repaint_event = gui_widget_repaint_event;
+    
+    return E_NO_ERROR;
+}
+
+void gui_widget_set_visible(gui_widget_t* widget, bool visible)
+{
+    widget->visible = visible;
+}
+
+void gui_widget_set_x(gui_widget_t* widget, graphics_pos_t x)
+{
+    widget->x = x;
+}
+
+void gui_widget_set_y(gui_widget_t* widget, graphics_pos_t y)
+{
+    widget->y = y;
+}
+
+void gui_widget_move(gui_widget_t* widget, graphics_pos_t x, graphics_pos_t y)
+{
+    widget->x = x;
+    widget->y = y;
+}
+
+void gui_widget_set_width(gui_widget_t* widget, graphics_size_t width)
+{
+    widget->width = width;
+}
+
+void gui_widget_set_height(gui_widget_t* widget, graphics_size_t height)
+{
+    widget->height = height;
+}
+
+void gui_widget_resize(gui_widget_t* widget, graphics_size_t width, graphics_size_t height)
+{
+    widget->width = width;
+    widget->height = height;
+}
--- gui/gui_widget.h
+++ gui/gui_widget.h
@@ -8,6 +8,8 @@
 #include "gui.h"
 #include "gui_object.h"
 #include "gui_event.h"
+#include "errors/errors.h"
+#include "defs/defs.h"
 #include <stdbool.h>
 #include <stddef.h>
 
@@ -18,30 +20,186 @@
 typedef struct _Gui_Widget gui_widget_t;
 #endif //GUI_WIDGET_TYPE_DEFINED
 
+//! Тип идентификатора виджета.
+typedef int widget_id_t;
+
 //! Структура виджета графического интерфейса.
 struct _Gui_Widget {
     gui_object_t super; //!< Суперкласс.
-    int id; //!< Идентификатор виджета.
+    widget_id_t id; //!< Идентификатор виджета.
     bool visible; //!< Флаг видимости.
-    int x; //!< Координата x.
-    int y; //!< Координата y.
-    size_t width; //!< Ширина.
-    size_t height; //!< Высота.
+    graphics_pos_t x; //!< Координата x.
+    graphics_pos_t y; //!< Координата y.
+    graphics_size_t width; //!< Ширина.
+    graphics_size_t height; //!< Высота.
     /**
      * Обработчик изменения размера.
      * @param widget Виджет.
      * @param event Событие изменения размера.
      */
-    void (*resize)(gui_widget_t* widget, gui_resize_event_t* event);
+    void (*resize_event)(gui_widget_t* widget, gui_resize_event_t* event);
     /**
      * Обработчик перерисовки.
      * @param widget Виджет.
      * @param event Событие перерисовки.
      */
-    void (*repaint)(gui_widget_t* widget, gui_repaint_event_t* event);
+    void (*repaint_event)(gui_widget_t* widget, gui_repaint_event_t* event);
 };
 
+//! Приводит указатель widget к типу виджета.
+#define GUI_WIDGET(widget) ((gui_widget_t*)(widget))
 
+/**
+ * Обработчик изменения размера по-умолчанию.
+ * @param widget Виджет.
+ * @param event Событие изменения размера.
+ */
+extern void gui_widget_resize_event(gui_widget_t* widget, gui_resize_event_t* event);
+
+/**
+ * Обработчик перерисовки по-умолчанию.
+ * @param widget Виджет.
+ * @param event Событие перерисовки.
+ */
+extern void gui_widget_repaint_event(gui_widget_t* widget, gui_repaint_event_t* event);
+
+/**
+ * Инициализирует виджет.
+ * @param widget Виджет.
+ * @param gui Графический интерфейс.
+ * @return Код ошибки.
+ */
+extern err_t gui_widget_init(gui_widget_t* widget, gui_t* gui);
+
+/**
+ * Инициализируют виджет как потомок заданного родителя.
+ * @param widget Виджет.
+ * @param gui Графический интерфейс.
+ * @param parent Родитель.
+ * @return Код ошибки.
+ */
+extern err_t gui_widget_init_parent(gui_widget_t* widget, gui_t* gui, gui_widget_t* parent);
+
+/**
+ * Получает идентификатор виджета.
+ * @param widget Виджет.
+ * @return Идентификатор виджета.
+ */
+ALWAYS_INLINE static widget_id_t gui_widget_id(gui_widget_t* widget)
+{
+    return widget->id;
+}
+
+/**
+ * Устанавливает идентификатор виджета.
+ * @param widget Виджет.
+ * @param id Идентификатор виджета.
+ */
+ALWAYS_INLINE static void gui_widget_set_id(gui_widget_t* widget, widget_id_t id)
+{
+    widget->id = id;
+}
+
+/**
+ * Получает флаг видимости виджета.
+ * @param widget Виджет.
+ * @return Флаг видимости виджета.
+ */
+ALWAYS_INLINE static bool gui_widget_visible(gui_widget_t* widget)
+{
+    return widget->visible;
+}
+
+/**
+ * Устанавливает флаг видимости виджета.
+ * @param widget Виджет.
+ * @param Флаг видимости виджета.
+ */
+extern void gui_widget_set_visible(gui_widget_t* widget, bool visible);
+
+/**
+ * Получает координату X виджета.
+ * @param widget Виджет.
+ * @return Координата X виджета.
+ */
+ALWAYS_INLINE static graphics_pos_t gui_widget_x(gui_widget_t* widget)
+{
+    return widget->x;
+}
+
+/**
+ * Получает координату Y виджета.
+ * @param widget Виджет.
+ * @return Координата Y виджета.
+ */
+ALWAYS_INLINE static graphics_pos_t gui_widget_y(gui_widget_t* widget)
+{
+    return widget->y;
+}
+
+/**
+ * Устанавливает координату X виджета.
+ * @param widget Виджет.
+ * @param x Координата X виджета.
+ */
+extern void gui_widget_set_x(gui_widget_t* widget, graphics_pos_t x);
+
+/**
+ * Устанавливает координату Y виджета.
+ * @param widget Виджет.
+ * @param y Координата Y виджета.
+ */
+extern void gui_widget_set_y(gui_widget_t* widget, graphics_pos_t y);
+
+/**
+ * Перемещает виджет.
+ * @param widget Виджет.
+ * @param x Координата X виджета.
+ * @param y Координата Y виджета.
+ */
+extern void gui_widget_move(gui_widget_t* widget, graphics_pos_t x, graphics_pos_t y);
+
+/**
+ * Получает ширину виджета.
+ * @param widget Виджет.
+ * @return Ширина виджета.
+ */
+ALWAYS_INLINE static graphics_size_t gui_widget_width(gui_widget_t* widget)
+{
+    return widget->width;
+}
+
+/**
+ * Получает высоту виджета.
+ * @param widget Виджет.
+ * @return Высота виджета.
+ */
+ALWAYS_INLINE static graphics_size_t gui_widget_height(gui_widget_t* widget)
+{
+    return widget->height;
+}
+
+/**
+ * Устанавливает ширину виджета.
+ * @param widget Виджет.
+ * @param width Ширина виджета.
+ */
+extern void gui_widget_set_width(gui_widget_t* widget, graphics_size_t width);
+
+/**
+ * Устанавливает высоту виджета.
+ * @param widget Виджет.
+ * @param height Высота виджета.
+ */
+extern void gui_widget_set_height(gui_widget_t* widget, graphics_size_t height);
+
+/**
+ * Изменяет размер виджета.
+ * @param widget Виджет.
+ * @param width Ширина виджета.
+ * @param height Высота виджета.
+ */
+extern void gui_widget_resize(gui_widget_t* widget, graphics_size_t width, graphics_size_t height);
 
 #endif	/* GUI_WIDGET_H */
 

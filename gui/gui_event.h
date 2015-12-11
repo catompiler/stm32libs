1a9ab96b1dd3975e730e56c6c01f288351f70ea2
artem.lab <artem.lab@gmail.com>
2015-12-11 10:43:33 +0500

    gui key event

--- gui/gui_event.h
+++ gui/gui_event.h
@@ -9,11 +9,14 @@
 #include <stddef.h>
 #include <stdbool.h>
 #include "graphics/rect.h"
+#include "input/key_input.h"
 
 typedef enum _Gui_Event_Type {
     GUI_EVENT_TYPE_NONE = 0, //!< Отсутствующее событие.
     GUI_EVENT_TYPE_RESIZE = 1, //!< Событие изменение размера.
-    GUI_EVENT_TYPE_REPAINT = 2 //!< Событие перерисовки.
+    GUI_EVENT_TYPE_REPAINT = 2, //!< Событие перерисовки.
+    GUI_EVENT_TYPE_KEY_PRESS = 3, //!< Событие нажатия клавиши.
+    GUI_EVENT_TYPE_KEY_RELEASE = 4 //!< Событие отпускания клавиши.
 } gui_event_type_t;
 
 //! Структура события графического интерфейса.
@@ -34,6 +37,12 @@ typedef struct _Gui_Repaint_Event {
     gui_event_t super; //!< Суперкласс.
     rect_t rect; //!< Область перерисовки.
 } gui_repaint_event_t;
+
+//! Структура события клавиатуры.
+typedef struct _Gui_Key_Event {
+    gui_event_t super; //!< Суперкласс.
+    key_t key; //!< Код клавиши.
+} gui_key_event_t;
 
 //! Приводит указатель event к типу события.
 #define GUI_EVENT(event) ((gui_event_t*)(event))
@@ -124,6 +133,18 @@ ALWAYS_INLINE static void gui_repaint_event_init_rect(gui_repaint_event_t* event
     rect_set_top(&event->rect, rect_top(rect));
     rect_set_right(&event->rect, rect_right(rect));
     rect_set_bottom(&event->rect, rect_bottom(rect));
+}
+
+ALWAYS_INLINE static void gui_key_press_event_init(gui_key_event_t* event, key_t key)
+{
+    gui_event_init(GUI_EVENT(event), GUI_EVENT_TYPE_KEY_PRESS);
+    event->key = key;
+}
+
+ALWAYS_INLINE static void gui_key_release_event_init(gui_key_event_t* event, key_t key)
+{
+    gui_event_init(GUI_EVENT(event), GUI_EVENT_TYPE_KEY_RELEASE);
+    event->key = key;
 }
 
 #endif	/* GUI_EVENT_H */

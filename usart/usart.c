c0dc8cffdf7b112cf2a855d11cfc6314c7bade2a
artem.lab <artem.lab@gmail.com>
2016-04-04 15:56:20 +0500

    usart.c & .h added for compatibility with old projects

---
+++ usart/usart.c
@@ -0,0 +1,4 @@
+#include "usart_buf.c"
+#include "usart_bus.c"
+
+#warning This file is deprecated! Use usart_buf.c or usart_bus.c instead!
---
+++ usart/usart.h
@@ -0,0 +1,4 @@
+#include "usart_buf.h"
+#include "usart_bus.h"
+
+#warning This file is deprecated! Use usart_buf.h or usart_bus.h instead!

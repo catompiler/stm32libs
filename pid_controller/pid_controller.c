1f2bd90f21e75e27da0d9e65f73d314f582506f1
artem.lab <artem.lab@gmail.com>
2017-04-13 12:03:59 +0500

    pid controller new clamp algorithm

--- pid_controller/pid_controller.c
+++ pid_controller/pid_controller.c
@@ -11,7 +11,7 @@ void pid_controller_init(pid_controller_t* controller, fixed32_t kp, fixed32_t k
     controller->prev_e = 0;
     controller->prev_i = 0;
     controller->value = 0;
-    controller->clamp_min = 0;
+    controller->clamp_min = INT32_MIN;
     controller->clamp_max = INT32_MAX;
 }
 
@@ -33,12 +33,35 @@ bool pid_controller_calculate(pid_controller_t* controller, fixed32_t e, fixed32
               i = ((int64_t)i * dt) >> FIXED32_FRACT_BITS;
               i = i + controller->prev_i;
     
-    // D = Kd * (e(t) - e(t-1) / dt;
+    // D = Kd * (e(t) - e(t-1)) / dt;
     fixed32_t d = ((int64_t)controller->kd * (e - controller->prev_e)) / dt;
     
+    /*
     p = CLAMP(p, -controller->clamp_max, controller->clamp_max);
     i = CLAMP(i, -controller->clamp_max, controller->clamp_max);
     d = CLAMP(d, -controller->clamp_max, controller->clamp_max);
+    */
+    
+    int32_t delta_min = INT32_MIN;
+    int32_t delta_max = INT32_MAX;
+    int64_t delta = (int64_t)controller->clamp_max - controller->clamp_min;
+    
+    if(delta < INT32_MAX){
+        delta_min = -(int32_t)delta;
+        delta_max = (int32_t)delta;
+    }
+    
+    p = CLAMP(p, delta_min, delta_max);
+    d = CLAMP(d, delta_min, delta_max);
+    
+    //delta_min -= p;
+    //delta_max -= p;
+    i = CLAMP(i, delta_min, delta_max);
+    
+    /*fixed32_t clamp_i_min = controller->clamp_min - p;
+    fixed32_t clamp_i_max = controller->clamp_max - p;
+    
+    i = CLAMP(i, clamp_i_min, clamp_i_max);*/
     
     controller->prev_e = e;
     controller->prev_i = i;
